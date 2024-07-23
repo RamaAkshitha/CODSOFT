4b27f62bde8ad99d1005bdd4c8b3cf147b6fd3cd
Akshitha <150222781+RamaAkshitha@users.noreply.github.com>
2024-07-23 19:44:13 +0530

    Update calculator.js

--- calculator(task-3)/calculator.js
+++ calculator(task-3)/calculator.js
@@ -1,21 +1,13 @@
 const display = document.querySelector('input[name="display"]');
-
-// Function to update the display value
 function updateDisplay(value) {
   display.value += value;
 }
-
-// Function to clear the display
 function clearDisplay() {
   display.value = '';
 }
-
-// Function to delete the last character from the display
 function deleteLastChar() {
   display.value = display.value.toString().slice(0, -1);
 }
-
-// Function to evaluate the expression and display the result
 function evaluateExpression() {
   try {
     const result = eval(display.value);
@@ -24,8 +16,6 @@ function evaluateExpression() {
     display.value = 'Error';
   }
 }
-
-// Add event listeners to the buttons
 document.querySelectorAll('input[type="button"]').forEach(button => {
   button.addEventListener('click', () => {
     const value = button.value;
